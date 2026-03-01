b93f006041ed00c6e480344505fd76797e250b71
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2026-03-01 23:32:46 +0100

    package: add calaos-remote-ui Buildroot package
    New br2-external package for the Calaos remote UI application targeting
    the 86Panel W display. Includes Config.in with dependency declarations
    and calaos-remote-ui.mk with build rules.

---
+++ buildroot-external/package/calaos-remote-ui/calaos-remote-ui.mk
@@ -0,0 +1,33 @@
+################################################################################
+#
+# calaos-remote-ui
+#
+################################################################################
+
+CALAOS_REMOTE_UI_VERSION ?= main
+CALAOS_REMOTE_UI_SITE ?= https://github.com/calaos/calaos_remote_ui
+CALAOS_REMOTE_UI_SITE_METHOD = git
+CALAOS_REMOTE_UI_GIT_SUBMODULES = YES
+
+CALAOS_REMOTE_UI_LICENSE = GPL-3.0
+CALAOS_REMOTE_UI_LICENSE_FILES = LICENSE
+
+CALAOS_REMOTE_UI_DEPENDENCIES = mbedtls libdrm libevdev host-python3
+
+CALAOS_REMOTE_UI_CONF_OPTS = \
+	-DBUILD_LINUX=ON \
+	-DBOARD=luckfox-86-panel \
+	-DCMAKE_BUILD_TYPE=Release
+
+define CALAOS_REMOTE_UI_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 $(CALAOS_REMOTE_UI_PKGDIR)/S99calaos \
+		$(TARGET_DIR)/etc/init.d/S99calaos
+endef
+
+define CALAOS_REMOTE_UI_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/build/calaos-remote-ui \
+		$(TARGET_DIR)/usr/bin/calaos-remote-ui
+	$(CALAOS_REMOTE_UI_INSTALL_INIT_SYSV)
+endef
+
+$(eval $(cmake-package))
